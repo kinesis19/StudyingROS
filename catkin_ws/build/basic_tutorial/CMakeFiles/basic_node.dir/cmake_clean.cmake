a07f5bae32c4f9ecfe02022679b1cecf8de40c59
kinesis19 <lordkinesis@naver.com>
2023-09-17 01:24:49 +0900

    Chapter2-2 ROS Node Add

---
+++ catkin_ws/build/basic_tutorial/CMakeFiles/basic_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/kinesis/Github/StudyingROS/catkin_ws/devel/lib/basic_tutorial/basic_node"
+  "/home/kinesis/Github/StudyingROS/catkin_ws/devel/lib/basic_tutorial/basic_node.pdb"
+  "CMakeFiles/basic_node.dir/src/basic.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_ws/build/basic_tutorial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kinesis/Github/StudyingROS/catkin_ws/src/basic_tutorial
+# Build directory: /home/kinesis/Github/StudyingROS/catkin_ws/build/basic_tutorial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
